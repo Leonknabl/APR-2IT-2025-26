32355cf5c4b1ef510d975fa80653253aae7bb46b
cat6849275 <businessxion2@gmail.com>
2025-09-23 08:38:34 +0200

    bruch und test klasse erweitert

--- src/k5/Bruch.java
+++ src/k5/Bruch.java
@@ -2,7 +2,20 @@ package k5;
 
 public class Bruch {
 
-    public static void main(String[] args) {
+
+    int zaehler = 1;
+    int nenner = 1;
+
+    //ausgeben
+    public void ausgeben() {
+        System.out.println(zaehler + "/" + nenner);
+    }
+
+    //erweitern
+    public void erweitern(int i) {
+        zaehler = zaehler * i;
+        nenner *= i;
+
 
     }
 }
--- src/k5/Test.java
+++ src/k5/Test.java
@@ -4,5 +4,18 @@ public class Test {
 
     public static void main(String[] args) {
 
+        Bruch b = new Bruch();
+        b.nenner = 4;
+        b.zaehler = 2;
+        System.out.println("Zähler: " + b.zaehler);
+        b.ausgeben();
+        b.erweitern(2);
+        b.ausgeben();
+        b.erweitern(3);
+        b.ausgeben();
+
+
+
+
     }
 }
